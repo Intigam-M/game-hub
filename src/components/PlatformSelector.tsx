0a72c6564d6aa61f4bbb1a60f7cf48b2248be2f7
Intigam Mammadli <intigam.mammadli@gmail.com>
2024-02-06 18:25:38 +0400

    Refactor- simplify game query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,27 +1,44 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
-import { BsChevronDown } from "react-icons/bs";
-import usePlatforms, { Platform } from "../hooks/usePlatforms";
-
-interface Props { 
-  onSelectPlatform: (platform: Platform) => void;
-  selectedPlatform: Platform | null;
-}
-
-const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
-  const { data, error } = usePlatforms();
-
-  if (error) return null;
+import {
+    Button,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
+  } from '@chakra-ui/react';
+  import { BsChevronDown } from 'react-icons/bs';
+  import usePlatforms, { Platform } from '../hooks/usePlatforms';
   
-  return (
-    <Menu>
-      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || 'Platforms'}
-      </MenuButton>
-      <MenuList>
-        {data?.results.map(platform => <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
-      </MenuList>
-    </Menu>
-  );
-};
-
-export default PlatformSelector;
+  interface Props {
+    onSelectPlatform: (platform: Platform) => void;
+    selectedPlatformId?: number;
+  }
+  
+  const PlatformSelector = ({
+    onSelectPlatform,
+    selectedPlatformId,
+  }: Props) => {
+    const { data, error } = usePlatforms();
+    const selectedPlatform = data?.results.find(p => p.id === selectedPlatformId);
+  
+    if (error) return null;
+  
+    return (
+      <Menu>
+        <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+          {selectedPlatform?.name || 'Platforms'}
+        </MenuButton>
+        <MenuList>
+          {data?.results.map((platform) => (
+            <MenuItem
+              onClick={() => onSelectPlatform(platform)}
+              key={platform.id}
+            >
+              {platform.name}
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
+    );
+  };
+  
+  export default PlatformSelector;
