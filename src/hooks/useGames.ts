cfc4cebf5d03600818d2143f4b7cdccb8427aba8
Intigam Mammadli <intigam.mammadli@gmail.com>
2024-02-06 18:49:46 +0400

    Refactor- simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,41 +1,39 @@
-import {
-    useInfiniteQuery,
-    useQuery,
-  } from '@tanstack/react-query';
-  import { GameQuery } from '../App';
-  import APIClient, {
-    FetchResponse,
-  } from '../services/api-client';
-  import { Platform } from './usePlatforms';
-  
-  const apiClient = new APIClient<Game>('/games');
-  
-  export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform }[];
-    metacritic: number;
-    rating_top: number;
-  }
-  
-  const useGames = (gameQuery: GameQuery) =>
-    useInfiniteQuery<FetchResponse<Game>, Error>({
-      queryKey: ['games', gameQuery],
-      queryFn: ({ pageParam = 1 }) =>
-        apiClient.getAll({
-          params: {
-            genres: gameQuery.genreId,
-            parent_platforms: gameQuery.platformId,
-            ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText,
-            page: pageParam,
-          },
-        }),
-      getNextPageParam: (lastPage, allPages) => {
-        return lastPage.next ? allPages.length + 1 : undefined;
-      },
-      staleTime: 24 * 60 * 60 * 1000, //24h
-    });
-  
-  export default useGames;
+import { useInfiniteQuery } from '@tanstack/react-query';
+import ms from 'ms';
+import { GameQuery } from '../App';
+import APIClient, {
+  FetchResponse,
+} from '../services/api-client';
+import { Platform } from './usePlatforms';
+
+const apiClient = new APIClient<Game>('/games');
+
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+  rating_top: number;
+}
+
+const useGames = (gameQuery: GameQuery) =>
+  useInfiniteQuery<FetchResponse<Game>, Error>({
+    queryKey: ['games', gameQuery],
+    queryFn: ({ pageParam = 1 }) =>
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genreId,
+          parent_platforms: gameQuery.platformId,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+          page: pageParam,
+        },
+      }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
+    staleTime: ms('24h')
+  });
+
+export default useGames;
